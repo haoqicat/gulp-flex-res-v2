8fbeb6f368bc2ab99f007a7a2dad9611fa955950
Peter Wang <happypeter1983@gmail.com>
2018-03-20 21:21:10 +0800

    6-gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,11 @@ const autoprefixer = require('gulp-autoprefixer')
 const imagemin = require('gulp-imagemin')
 const pngquant = require('imagemin-pngquant')
 
+function handleError(err) {
+  console.log(err.toString())
+  this.emit('end')
+}
+
 gulp.task('imagemin', function() {
   return gulp
     .src('src/img/*')
@@ -19,6 +24,7 @@ gulp.task('sass', function() {
   gulp
     .src('src/css/main.scss')
     .pipe(sass())
+    .on('error', handleError)
     .pipe(
       autoprefixer({
         browsers: ['last 2 versions']
@@ -31,4 +37,9 @@ gulp.task('copy', function() {
   gulp.src('src/*.html').pipe(gulp.dest('dist/'))
 })
 
-gulp.task('default', ['sass', 'copy'])
+gulp.task('watch', function() {
+  gulp.watch(['src/*.html'], ['copy'])
+  gulp.watch(['src/css/*.scss'], ['sass'])
+})
+
+gulp.task('default', ['sass', 'copy', 'watch'])
